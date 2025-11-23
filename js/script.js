a8d06d7f6e8b28dbb6fd6953d30de76aebb12bd1
Pat <dmtpatricica@email.com>
2025-11-23 23:40:35 +0100

    api connected

--- js/script.js
+++ js/script.js
@@ -1,47 +1,10 @@
-const teamMembers = [
-  {
-    name: "Marco Bianchi",
-    role: "Designer",
-    email: "marcobianchi@team.com",
-    img: "img/male1.png"
-  },
-  {
-    name: "Laura Rossi",
-    role: "Front-end Developer",
-    email: "laurarossi@team.com",
-    img: "img/female1.png"
-  },
-  {
-    name: "Giorgio Verdi",
-    role: "Back-end Developer",
-    email: "giorgioverdi@team.com",
-    img: "img/male2.png"
-  },
-  {
-    name: "Marta Ipsum",
-    role: "SEO Specialist",
-    email: "martarossi@team.com",
-    img: "img/female2.png"
-  },
-  {
-    name: "Roberto Lorem",
-    role: "SEO Specialist",
-    email: "robertolorem@team.com",
-    img: "img/male3.png"
-  },
-  {
-    name: "Daniela Amet",
-    role: "Analyst",
-    email: "danielaamet@team.com",
-    img: "img/female3.png"
-  }
-];
+axios.get('https://boolean-teachers.github.io/mock/api/members/').then((response)=>{
 
-let allTeamsCard = "";
-
-teamMembers.forEach(element => {
-  allTeamsCard +=
-    `<div class="column card rounded-0 ms-0 ps-0  bg-black text-white" style="width: calc(100% / 3 - 2rem);">
+  let allTeamsCard = "";
+  console.log(response.data);
+  response.data.forEach(element => {
+    allTeamsCard +=
+      `<div class="column card rounded-0 ms-0 ps-0  bg-black text-white" style="width: calc(100% / 3 - 2rem);">
                     <div class="row ms-0 g-0">
                         <div class="col-4">
                           <img src=${element.img} class="img-fluid" alt="${element.name}">
@@ -58,6 +21,7 @@ teamMembers.forEach(element => {
                     </div>
                 </div>`;
   
-});
-document.getElementById("cards-container").innerHTML = allTeamsCard;
+  });
+  document.getElementById("cards-container").innerHTML = allTeamsCard;
 
+});
